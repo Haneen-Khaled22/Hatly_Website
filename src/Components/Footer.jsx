f929f22a785b0314035a7177732b2fb31d4cf049
Haneen-Khaled22 <haneenk385@gmail.com>
2025-05-08 03:23:40 +0300

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,7 +14,7 @@ import PhoneIcon from "@mui/icons-material/Phone";
 
 function Footer() {
   const footerLinks = [
-    
+
     {
       title: "App",
       links: ["Trade", "Download"],
@@ -475,8 +475,8 @@ function Footer() {
             }}
           >
             <Link href="#">Cookie policy</Link>
-            <Link href="/privacy-policy">Privacy policy</Link>
-            <Link href="/terms-of-service">Terms of service</Link>
+            <Link href="/privacy-policy" target='_blank'>Privacy policy</Link>
+            <Link href="/terms-of-service" target='_blank'>Terms of service</Link>
             <Link href="#">Help center</Link>
           </Stack>
         </Stack>
