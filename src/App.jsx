5c4ea6e4b351c91fae825f0d0e127402e3f4222f
Haneen-Khaled22 <haneenk385@gmail.com>
2025-05-20 23:19:06 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import {  Route, Routes } from 'react-router-dom'
-import './index.css'
-import About from './Components/About'
-
-import Contact from './Components/Contact'
-import Home from './Components/Home'
-
-import Footer from './Components/Footer'
-import { useEffect, useState } from 'react'
-import Loader from './Components/Loader/Loader'
-import TemporaryDrawer from './Components/Drawer'
-import FrequentlyAsked from './Components/F&Qs'
-import TermsOfService from './Components/TermsOfService'
-import PrivacyPolicy from './Components/PrivacyPolicy'
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import { useEffect, useState } from 'react';
+import Loader from './Components/Loader/Loader';
+import TemporaryDrawer from './Components/Drawer';
+import FrequentlyAsked from './Components/F&Qs';
+import TermsOfService from './Components/TermsOfService';
+import PrivacyPolicy from './Components/PrivacyPolicy';
+import WhatsAppButton from './Components/WhatsAppButton';
 
 function App() {
-
-  // loader state
   const [isLoading, setIsLoading] = useState(true);
-
-  // Let create async method to fetch fake data
-
-    useEffect(()=>{
-    setIsLoading(false);
-
-  },[]);
 
   useEffect(() => {
     const fakeDataFetch = () => {
@@ -31,40 +21,29 @@ function App() {
         setIsLoading(false);
       }, 4000);
     };
-    
 
     fakeDataFetch();
   }, []);
 
-
-
   return isLoading ? (
     <Loader />
   ) : (
-    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", }}>
-      {/* <ResponsiveAppBar />  */}
-      <TemporaryDrawer/>
-    
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      <TemporaryDrawer />
 
       <Routes>
-       
-        
-        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
-         <Route path="/" element={<Home />} />
-          <Route path="/frequently-asked" element={<FrequentlyAsked />} />
-          
-          <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/contact-us" element={<Contact/>} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/frequently-asked" element={<FrequentlyAsked />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
-      
-      
 
-      <Footer /> 
+      <Footer />
+      <WhatsAppButton />
     </div>
   );
 }
 
-
-export default App
+export default App;
