15782417057110bd063c9eeb2f89c65c5cf36ef1
Haneen-Khaled22 <haneenk385@gmail.com>
2025-05-21 03:02:23 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return  (
     <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-      <TemporaryDrawer />
+     {isLoading?null: <TemporaryDrawer />}
 
       <Routes>
         
